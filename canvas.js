c24e3efafa4859d15263e0105560328d86ab3d02
S1mkin <s.a.s.s.e.r@mail.ru>
2020-02-20 10:02:19 +0300

    add onclick

--- canvas.js
+++ canvas.js
@@ -1,32 +1,16 @@
-
-/*
-var example = document.getElementById("jsCubes");
-
-var ctx = example.getContext('2d');
-ctx.fillRect(0, 0, example.width, example.height);
-
-console.log('example.width: ' + example.width);
-*/
-
 class Canvas {
 
-    constructor(id, idParentElement, width, height) {
+    constructor(id, idParentElement, width, height, step) {
         this.canvas = document.createElement('canvas');
         this.canvas.ctx = this.canvas.getContext('2d');
         this.canvas.id = id;
         this.canvas.height = height;
         this.canvas.width = width;
         this.canvas.idParentElement = idParentElement;
-        this.step = 40;
-        this.CREATE_CANVAS();      
-    }
+        this.step = step;
+        this.colors = ["#fff", "#db1414", "#23518c", "#276f21", "#c1af1b"];
 
-    RANDOM_COLOR() {
-        let colors = ["#F00", "#955", "#044", "#F08", "#208", "#8F8", "#0F0", "#0A0", "#040", "#00F", "#456"];
-        function getRandomInt(min, max) {
-            return Math.floor(Math.random() * (max - min)) + min;
-        }
-        return colors[getRandomInt(0, colors.length)];  
+        this.CREATE_CANVAS();
     }
 
     CREATE_CANVAS() {
@@ -43,15 +27,68 @@ class Canvas {
         this.canvas.ctx.fillRect(start_x, start_y, this.step - 1, this.step - 1);
     }
 
-}
-
-
-const jsCubes = new Canvas("jsCubes", "jsCubes-wrap", 400, 400);
-
-for (let x = 1; x <= 10; x++) {
-    for (let y = 1; y <= 10; y++) {
-        jsCubes.PAINT_CUBES(x, y, jsCubes.RANDOM_COLOR());
+    REPAINT_FIELDS(fields) {
+        fields.forEach(el => {
+            this.PAINT_CUBES(el.x, el.y, this.colors[el.value]);
+        });
     }
+
+    COORD_TO_POSITION(x, y) {
+        return {
+            x: Math.ceil(x / this.step),
+            y: Math.ceil(y / this.step)
+        }
+    }
+
 }
 
-//jsCubes-wrap
+
+class jsCubes {
+    constructor(xCount, yCount) {
+        this.xCount = xCount;
+        this.yCount = yCount;
+        this.fields = [];
+
+        this.CREATE_FIELDS();
+    }
+
+    CREATE_FIELDS() {
+        for (let x = 1; x <= this.xCount; x++) {
+            for (let y = 1; y <= this.yCount; y++) {
+                this.fields.push({
+                    x, y, value: this.RANDOM_VALUE()
+                });
+            }
+        }
+    }
+
+    RANDOM_VALUE() {
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min)) + min;
+        }
+        return getRandomInt(1, 5);
+    }
+
+    FIELD_VALUE(x, y, newValue) {
+        this.fields.forEach(el => {
+            if (el.x === x && el.y === y) {
+                el.value = newValue;
+            }
+        });
+    }
+
+
+
+}
+
+
+const jsCubesFields = new jsCubes(10, 10);
+const jsCubesCanvas = new Canvas("jsCubes", "jsCubes-wrap", 400, 400, 40);
+
+jsCubesCanvas.REPAINT_FIELDS(jsCubesFields.fields);
+
+jsCubesCanvas.canvas.onclick = function(event){
+    let position = jsCubesCanvas.COORD_TO_POSITION(event.offsetX, event.offsetY);
+    jsCubesFields.FIELD_VALUE(position.x, position.y, 0);
+    jsCubesCanvas.REPAINT_FIELDS(jsCubesFields.fields);
+};
